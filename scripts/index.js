bdc07c9daaf8a9b09fea61f2acdb9539991df2b9
JXiang0046 <jchu0046@student.monash.edu>
2020-09-17 15:28:35 +0800

    Fixed minor issue with index.js
    changed = to : and changed formatting of date to look more tidy

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const firebaseRef = firebase.database().ref()
+const firebaseReference = firebase.database().ref()
 
 function create(){
     var projectName = String(document.getElementById("Project Name").value),
@@ -10,24 +10,24 @@ function create(){
         startMonth = String(document.getElementById("start_month").value),
         startYear = String(document.getElementById("Start Year").value),
         //temporary
-        startDate = String(document.getElementById("Start Day").value + " " + document.getElementById("start_month").value + " " + document.getElementById("Start Year").value),
+        startDate = String(document.getElementById("Start Day").value + "/" + document.getElementById("start_month").value + "/" + document.getElementById("Start Year").value),
 
         //project end
         endDay = String(document.getElementById("End Day").value),
         endMonth = String(document.getElementById("end_month").value),
         endYear = String(document.getElementById("End Year").value),
         //temporary
-        endDate = String(document.getElementById("End Day").value + " " + document.getElementById("end_month").value + " " + document.getElementById("End Year").value),
+        endDate = String(document.getElementById("End Day").value + "/" + document.getElementById("end_month").value + "/" + document.getElementById("End Year").value)
 
     if (description.length == 0){
-        description = "Please edit description.";
+        description = "N/A";
     }
 
-    firebaseRef.child(`Projects/${projectName}`).set({
-		ProjectName = projectName,
-        Description = description,
-        StartDate = startDate,
-        EndDate = endDate,
+    firebaseReference.child(`Projects/${projectName}`).set({
+		ProjectName: projectName,
+        Description: description,
+        StartDate: startDate,
+        EndDate: endDate,
 	})
 
     window.alert("Project Created!")
