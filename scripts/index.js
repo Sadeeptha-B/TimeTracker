93cdaf07131f8a7ff298d5dff0a8721f76aa63c9
JXiang0046 <jchu0046@student.monash.edu>
2020-09-17 15:08:44 +0800

    added index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,36 @@
+const firebaseRef = firebase.database().ref()
 
+function create(){
+    var projectName = String(document.getElementById("Project Name").value),
+        description = String(document.getElementById("Description").value),
+        //students =  String(document.getElementById("Students").value),
+
+        //project start
+        startDay =  String(document.getElementById("Start Day").value),
+        startMonth = String(document.getElementById("start_month").value),
+        startYear = String(document.getElementById("Start Year").value),
+        //temporary
+        startDate = String(document.getElementById("Start Day").value + " " + document.getElementById("start_month").value + " " + document.getElementById("Start Year").value),
+
+        //project end
+        endDay = String(document.getElementById("End Day").value),
+        endMonth = String(document.getElementById("end_month").value),
+        endYear = String(document.getElementById("End Year").value),
+        //temporary
+        endDate = String(document.getElementById("End Day").value + " " + document.getElementById("end_month").value + " " + document.getElementById("End Year").value),
+
+    if (description.length == 0){
+        description = "Please edit description.";
+    }
+
+    firebaseRef.child(`Projects/${projectName}`).set({
+		ProjectName = projectName,
+        Description = description,
+        StartDate = startDate,
+        EndDate = endDate,
+	})
+
+    window.alert("Project Created!")
+    // Bring the user to the home page after successful sign up
+    window.location.href = "../html/home.html";
+}
